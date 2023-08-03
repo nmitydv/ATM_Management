f2d032fc202e2c8cde276986db66eb63376f89dc
Namit Khushal Yadav <yadavnamit605@gmail.com>
2023-08-03 18:03:02 +0530

    HALF COMPLETED

---
+++ src/atm_machine/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atm_machine;
+
+/**
+ *
+ * @author 91975
+ */
+public class Main {
+    public static void main(String[] args) {
+        First f = new First();
+    }
+}
